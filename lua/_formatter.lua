46c835b1aafc24d741899c238cae7452d626fb45
22mahmoud <mahmoudss1997@gmail.com>
2020-10-13 18:56:23 +0200

    formatter

---
+++ lua/_formatter.lua
@@ -0,0 +1,38 @@
+-- load packages
+vim.cmd("packadd formatter.nvim")
+
+local map = require("utils").map
+local api = vim.api
+
+local prettier = {
+  prettier = function()
+    return {
+      exe = "prettier",
+      args = {"--stdin-filepath", api.nvim_buf_get_name(0)},
+      stdin = true
+    }
+  end
+}
+
+require("format").setup(
+  {
+    javascript = prettier,
+    javascriptreact = prettier,
+    typescript = prettier,
+    typescriptreact = prettier,
+    css = prettier,
+    html = prettier,
+    lua = {
+      luafmt = function()
+        return {
+          exe = "luafmt",
+          args = {"--indent-count", 2, "--stdin"},
+          stdin = true
+        }
+      end
+    }
+  }
+)
+
+-- vim.api.nvim_command('autocmd BufWrite * :Format')
+map("n", ",f", ":Format<CR>", {silent = true})
