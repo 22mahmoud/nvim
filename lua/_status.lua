a5880f8d6e69a3b350adb108a81e829a7e0788dc
22mahmoud <mahmoudss1997@gmail.com>
2020-10-17 15:48:04 +0200

    status

--- lua/_status.lua
+++ lua/_status.lua
@@ -4,17 +4,27 @@ local M = {}
 
 function M.setup()
   local line = [[]]
+
+  -- file path
+  line = line .. [[%#TermCursor#]]
   line =
     line ..
     [[ %{luaeval("require('nvim-web-devicons').get_icon(vim.fn.expand('%:e')) or '' ")} ]]
   line = line .. [[%2{&modified ? '● ' : ''}]]
-  line = line .. [[%{expand('%:~:.')}]] -- relative filepath
-  line = line .. [[%2{&readonly ? '' : ''}]]
-  line = line .. [[ (Ln %l/%L, Col %c) ]]
+  line = line .. [[%{expand('%:~:.')} ]] -- relative filepath
+  line = line .. [[%#StatusLine#]]
+
 
   -- rhs
   line = line .. [[ %= ]]
-  -- line = line .. [[ %y ]] -- filetype
+
+  -- col and line
+  line = line .. [[%#Title#]]
+  line = line .. [[ (Ln %l/%L, Col %c) ]]
+
+  line = line .. [[%#StatusLine#]]
+
+  line = line .. [[ %y ]] -- filetype
 
   o.laststatus = 2
   o.statusline = line
