371ae1147ff47a913eddd8c77781de14162791a5
22mahmoud <mahmoudss1997@gmail.com>
2020-11-11 11:00:10 +0200

    add new colorschemes

--- lua/_theme.lua
+++ lua/_theme.lua
@@ -1,3 +1,5 @@
 vim.o.background = "dark"
 vim.g.gruvbox_contrast_dark = "hard"
 vim.api.nvim_command("colorscheme gruvbox")
+-- vim.api.nvim_command("colorscheme moonfly")
+-- vim.api.nvim_command("colorscheme nightfly")
