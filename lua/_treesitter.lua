a5d780b48b19ed597632f36c8b1e155c911fcb3e
22mahmoud <hello@mahmoudashraf.dev>
2020-11-27 11:56:17 +0200

    pkg updts

--- lua/_treesitter.lua
+++ lua/_treesitter.lua
@@ -1,5 +1,5 @@
 require "nvim-treesitter.configs".setup {
-  ensure_installed = "all",
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true
   }
