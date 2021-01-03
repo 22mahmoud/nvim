7be99c10961f4fa59ccc998442b8e38b16f64c10
22mahmoud <hello@mahmoudashraf.dev>
2021-01-04 01:05:56 +0200

    updates

--- lua/_diagnostic.lua
+++ lua/_diagnostic.lua
@@ -5,7 +5,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
-    underline = false,
+    underline = true,
     signs = true,
     update_in_insert = false,
     virtual_text = {
