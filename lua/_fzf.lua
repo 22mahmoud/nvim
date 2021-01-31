3ab4e95d8462b324c91a9cb8801617c43be804ae
22mahmoud <mahmoud.ashraf@roadtripnation.org>
2021-01-31 18:59:00 +0200

    fzf colors

--- lua/_fzf.lua
+++ lua/_fzf.lua
@@ -1,6 +1,22 @@
 local g = vim.g
 local map = vim.api.nvim_set_keymap
 
+g.fzf_colors = {
+  fg = {"fg", "Normal"},
+  bg = {"bg", "Normal"},
+  hl = {"fg", "Comment"},
+  ["fg+"] = {"fg", "CursorLine", "CursorColumn", "Normal"},
+  ["bg+"] = {"bg", "CursorLine", "CursorColumn"},
+  ["hl+"] = {"fg", "Statement"},
+  info = {"fg", "PreProc"},
+  border = {"fg", "Ignore"},
+  prompt = {"fg", "Conditional"},
+  pointer = {"fg", "Exception"},
+  marker = {"fg", "Keyword"},
+  spinner = {"fg", "Label"},
+  header = {"fg", "Comment"}
+}
+
 g.fzf_action = {
   ["ctrl-s"] = "sp",
   ["ctrl-v"] = "vs",
