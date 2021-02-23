c036ccb41bac6e7f77c8fa8c31fd0b2cbbc96c2d
22mahmoud <mahmoud.ashraf@roadtripnation.org>
2021-02-23 16:37:00 +0200

    tree fix

--- lua/_tree.lua
+++ lua/_tree.lua
@@ -3,6 +3,7 @@ local map = vim.fn.nvim_set_keymap
 local g = vim.g
 
 g.nvim_tree_side = "left"
+g.nvim_tree_disable_keybindings = 1
 g.nvim_tree_width = 25
 g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
 g.nvim_tree_auto_open = 0
