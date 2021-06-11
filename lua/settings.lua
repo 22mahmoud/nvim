ed09b2c6d1a8a646eeb47c8e36d312076ab04efd
22mahmoud <hello@mahmoudashraf.dev>
2021-06-11 19:40:09 +0200

    minimal config

---
+++ lua/settings.lua
@@ -0,0 +1,145 @@
+vim.cmd[[colorscheme gruvbox8_hard]]
+
+local indentation = {
+  wrap = false,
+  softtabstop = 2,
+  textwidth = 80,
+  shiftwidth = 2,
+  expandtab = true,
+  autoindent = true,
+  shiftround = true,
+}
+
+local timings = {
+  updatetime = 300,
+  timeout = true,
+  timeoutlen = 500,
+  ttimeoutlen = 10
+}
+
+local win_buff = {
+  hidden = true,
+  splitbelow = true,
+  splitright = true,
+}
+
+local grep = {
+  grepprg = [[rg --hidden --glob "!.git" --no-heading --smart-case --vimgrep --follow $*]],
+  grepformat = {"%f:%l:%c:%m"}
+}
+
+local wildmode = {
+  wildcharm = vim.fn.char2nr [[\<C-Z>]],
+  wildmode = 'full',
+  wildoptions = 'pum',
+  wildignorecase = true,
+  wildignore = {
+    "*.aux",
+    "*.out",
+    "*.toc",
+    "*.o",
+    "*.obj",
+    "*.dll",
+    "*.jar",
+    "*.pyc",
+    "*.rbc",
+    "*.class",
+    "*.gif",
+    "*.ico",
+    "*.jpg",
+    "*.jpeg",
+    "*.png",
+    "*.avi",
+    "*.wav",
+    "*.webm",
+    "*.eot",
+    "*.otf",
+    "*.ttf",
+    "*.woff",
+    "*.doc",
+    "*.pdf",
+    "*.zip",
+    "*.tar.gz",
+    "*.tar.bz2",
+    "*.rar",
+    "*.tar.xz",
+    -- Cache
+    ".sass-cache",
+    "*/vendor/gems/*",
+    "*/vendor/cache/*",
+    "*/.bundle/*",
+    "*.gem",
+    "*/node_modules/*",
+    -- Temp/System
+    "*.*~",
+    "*~ ",
+    "*.swp",
+    ".lock",
+    ".DS_Store",
+    "._*",
+    "tags.lock"
+  }
+}
+
+local display = {
+  conceallevel = 2,
+  breakindentopt = "sbr",
+  linebreak = true,
+  signcolumn = 'auto',
+  ruler = false,
+  colorcolumn = {"+1"},
+  list = true,
+  listchars = {
+    eol = " ",
+    tab = "│ ",
+    extends = "»",
+    precedes = "«",
+    trail = "•"
+  }
+}
+
+local search = {
+  hlsearch = false,
+  incsearch = true,
+  smartcase = true,
+  ignorecase = true,
+}
+
+local general = {
+  clipboard = "unnamedplus",
+  termguicolors = true,
+  encoding = 'utf-8',
+  inccommand = "split",
+}
+
+local backup = {
+  backup = false,
+  writebackup = false,
+  swapfile = false,
+  undofile = true,
+  undodir = "~/.local/share/nvim/undo"
+}
+
+local mouse = {
+  mouse = "a",
+  mousefocus = true
+}
+
+local options = vim.tbl_deep_extend(
+  "force",
+  indentation,
+  timings,
+  win_buff,
+  grep,
+  wildmode,
+  display,
+  search,
+  general,
+  backup,
+  mouse,
+  vim.opt
+)
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
