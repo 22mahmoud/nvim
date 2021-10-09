93ffdd8a2d595a276c0bd7059337ee3e8da6f542
22mahmoud <hello@mahmoudashraf.dev>
2021-10-09 20:57:07 +0200

    add mode & filepath into statusline

--- lua/ma/statusline.lua
+++ lua/ma/statusline.lua
@@ -1,12 +1,56 @@
 local utils = require("ma.utils")
 
 local augroup = utils.augroup
+local fmt = string.format
+local fn = vim.fn
 
 local M = {}
 _G._.statusline = M
 
+local modes = {
+  n = "NORMAL",
+  i = "INSERT",
+  R = "REPLACE",
+  v = "VISUAL",
+  V = "V-LINE",
+  c = "COMMAND",
+  [""] = "V-BLOCK",
+  s = "SELECT",
+  S = "S-LINE",
+  [""] = "S-BLOCK",
+  t = "TERMINAL"
+}
+
+local function get_mode()
+  return modes[vim.fn.mode()] or ""
+end
+
+local function get_path()
+  local file_name = fn.expand("%:~:.:t")
+  local base = fn.expand("%:~:.:h"):gsub("/$", "") .. "/"
+  local path = base .. file_name
+
+  if path == "/" then
+    return ""
+  end
+
+  local max_len = math.min(35, math.floor(0.6 * fn.winwidth(0)))
+
+  if (#path + #file_name) > max_len then
+    return fn.pathshorten(path)
+  end
+
+  return path
+end
+
 function M.get_active_statusline()
-  return "%f"
+  local mode = get_mode()
+  local path = get_path()
+
+  return table.concat {
+    mode ~= "" and fmt("[%s] ", mode) or "",
+    path
+  }
 end
 
 function M.get_inactive_statusline()
