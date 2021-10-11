e000939e831c22cde9c6caa45f71f853501edbb1
22mahmoud <hello@mahmoudashraf.dev>
2021-10-11 12:51:35 +0200

    Squashed commit of the following:
    commit 88b88985ba8f524b670d6315796f0bc3a4f7d1ec
    Author: 22mahmoud <hello@mahmoudashraf.dev>
    Date:   Mon Oct 11 12:50:14 2021 +0200
    
        add more functions
    
    commit 93ffdd8a2d595a276c0bd7059337ee3e8da6f542
    Author: 22mahmoud <hello@mahmoudashraf.dev>
    Date:   Sat Oct 9 20:57:07 2021 +0200
    
        add mode & filepath into statusline
    
    commit 0a75c3705dfd5ed7eb40f51f4f4d2bcdaba2470b
    Author: 22mahmoud <hello@mahmoudashraf.dev>
    Date:   Sat Oct 9 20:55:55 2021 +0200
    
        basic setup for statuline
    
    commit aa0de85d9b97834df4abf7ca195873b275e98090
    Author: 22mahmoud <hello@mahmoudashraf.dev>
    Date:   Sat Oct 9 20:54:18 2021 +0200
    
        setup global lua object

---
+++ lua/ma/statusline.lua
@@ -0,0 +1,121 @@
+local utils = require("ma.utils")
+
+local augroup = utils.augroup
+local fmt = string.format
+local fn = vim.fn
+
+local M = {}
+_G._.statusline = M
+
+local modes = {
+  n = "NORMAL",
+  i = "INSERT",
+  R = "REPLACE",
+  v = "VISUAL",
+  V = "V-LINE",
+  c = "COMMAND",
+  [""] = "V-BLOCK",
+  s = "SELECT",
+  S = "S-LINE",
+  [""] = "S-BLOCK",
+  t = "TERMINAL"
+}
+
+local function get_mode()
+  return modes[vim.fn.mode()] or ""
+end
+
+local function get_path()
+  local file_name = fn.expand("%:~:.:t")
+  local base = fn.expand("%:~:.:h"):gsub("/$", "") .. "/"
+  local path = base .. file_name
+
+  if path == "/" then
+    return ""
+  end
+
+  local max_len = math.min(35, math.floor(0.6 * fn.winwidth(0)))
+
+  if (#path + #file_name) > max_len then
+    return fn.pathshorten(path)
+  end
+
+  return path
+end
+
+local function modified()
+  return vim.opt.modified:get() and "●" or ""
+end
+
+local function readonly()
+  local mod = vim.opt.modifiable:get()
+  local ro = vim.opt.readonly:get()
+
+  if (mod and not ro) then
+    return ""
+  end
+
+  return (ro and mod) and "" or ""
+end
+
+local function sep(value, template)
+  if (value == "" or value == nil or not value) then
+    return ""
+  end
+
+  return fmt((template or "%s") .. " ", value)
+end
+
+function M.get_active_statusline()
+  local mode = get_mode()
+  local path = get_path()
+  local modified_icon = modified()
+  local readonly_icon = readonly()
+
+  local lhs =
+    table.concat {
+    sep(mode, "[%s]"),
+    sep(path),
+    sep(modified_icon),
+    sep(readonly_icon)
+  }
+
+  local rhs =
+    table.concat {
+    sep("%l/%c"),
+  }
+
+  return lhs .. "%=" .. rhs
+end
+
+function M.get_inactive_statusline()
+  return [[%f %y %m]]
+end
+
+local function active()
+  vim.opt.statusline = [[%!luaeval("_.statusline.get_active_statusline()")]]
+end
+
+local function inactive()
+  vim.opt.statusline = [[%!luaeval("_.statusline.get_inactive_statusline()")]]
+end
+
+local function setup()
+  augroup(
+    "StatusLine",
+    {
+      {
+        events = {"WinEnter", "BufEnter"},
+        targets = {"*"},
+        command = active
+      },
+      {
+        events = {"WinLeave", "BufLeave"},
+        targets = {"*"},
+        command = inactive
+      }
+    }
+  )
+end
+
+setup()
