8dad56166a5666d67012ee89669c4e2891cdfefd
22mahmoud <hello@mahmoudashraf.dev>
2021-10-20 21:38:15 +0200

    disable more not needed plugins

--- init.lua
+++ init.lua
@@ -2,14 +2,25 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
 -- skip vim plugins
-vim.g.did_install_default_menus = 1
+vim.g.loaded_2html_plugin = 1
 vim.g.loaded_getscript = 1
 vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_logipat = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
 vim.g.loaded_vimball = 1
 vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 
 _G.G = {}
 
