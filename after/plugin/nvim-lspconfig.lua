0d8d18b6db6e11115246ebe56a8be11ad20ec32f
22mahmoud <hello@mahmoudashraf.dev>
2021-10-26 15:04:41 +0200

    add js/ts auto import on complete

--- after/plugin/nvim-lspconfig.lua
+++ after/plugin/nvim-lspconfig.lua
@@ -11,6 +11,42 @@ lsp.setup {
       lsp.on_attach(client, bufnr)
 
       client.resolved_capabilities.document_formatting = false
+
+      G.augroup('TSLspImportOnCompletion', {
+        {
+          events = { 'CompleteDone' },
+          targets = { '<buffer>' },
+          command = function()
+            local completed_item = vim.v.completed_item
+            if
+              not (
+                completed_item
+                and completed_item.user_data
+                and completed_item.user_data.nvim
+                and completed_item.user_data.nvim.lsp
+                and completed_item.user_data.nvim.lsp.completion_item
+              )
+            then
+              return
+            end
+
+            local item = completed_item.user_data.nvim.lsp.completion_item
+
+            vim.lsp.buf_request(
+              bufnr,
+              'completionItem/resolve',
+              item,
+              function(_, result)
+                if not (result and result.additionalTextEdits) then
+                  return
+                end
+
+                vim.lsp.util.apply_text_edits(result.additionalTextEdits, bufnr)
+              end
+            )
+          end,
+        },
+      })
     end,
     root_dir = root_pattern(
       'package.json',
