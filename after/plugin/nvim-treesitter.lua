c8bbbf2b5214409dd68111a3d4fb3cec33d45c04
Mahmoud Ashraf <mashraf@lynks.com>
2022-06-01 11:37:59 +0200

    update

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'all',
+  ignore_install = { "phpdoc" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
