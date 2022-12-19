cbf03352ddc8cd0550d9db9d542064200474da8b
22mahmoud <hello@mahmoudashraf.dev>
2022-12-19 19:45:14 +0200

    treesitter update

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,9 +1,7 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'all',
-  ignore_install = { "phpdoc" },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = true,
   },
   indent = {
     enable = true,
