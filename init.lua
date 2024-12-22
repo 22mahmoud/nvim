6ff3ea5190bb4d221d5763e4ef990aa9b5b621de
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2024-12-22 11:58:30 +0200

    enhance ricing

--- init.lua
+++ init.lua
@@ -12,9 +12,6 @@ require 'ma.statusline'
 require 'ma.plugins'
 require 'ma.highlight'
 
-local rc = vim.fs.joinpath(vim.env.XDG_CONFIG_HOME, 'nvimrc.lua')
-if vim.uv.fs_stat(rc) then vim.cmd(string.format('so %s', rc)) end
-
 -- load .nvimrc manually
 local project_marker = { '.nvimrc.lua' }
 local project_root = vim.fs.root(0, project_marker) or vim.env.HOME
--- lua/ma/highlight.lua
+++ lua/ma/highlight.lua
@@ -1,5 +1,8 @@
 local statusline = require 'ma.statusline'
 
+local colorscheme_rc = vim.fs.joinpath(vim.env.XDG_CONFIG_HOME, 'ricing', 'colorscheme.lua')
+if vim.uv.fs_stat(colorscheme_rc) then vim.cmd(string.format('so %s', colorscheme_rc)) end
+
 local function user_highlights()
   local opts = { guibg = nil }
 
