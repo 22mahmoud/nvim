7ca7b75d9f13399ef2fc9a467d96ba4d62cf9bfe
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2026-02-06 18:03:47 +0200

    use mise instead of volta

--- lua/ma/providers.lua
+++ lua/ma/providers.lua
@@ -8,25 +8,22 @@ vim.g.loaded_perl_provider = 0
 
 -- python3
 vim.g.python3_host_skip_check = 1
-if vim.fn.executable 'python3' then
+if vim.fn.executable 'python3' == 1 then
   vim.g.python3_host_prog = vim.fn.exepath 'python3'
 else
   vim.g.loaded_python3_provider = 0
 end
 
 -- node
-if vim.fn.executable 'neovim-node-host' then
-  if vim.fn.executable 'volta' then
-    vim.g.node_host_prog = vim.fn.trim(vim.fn.system 'volta which neovim-node-host')
-  else
-    vim.g.node_host_prog = vim.fn.exepath 'neovim-node-host'
-  end
+local node_host = vim.fn.expand '~/.local/share/npm/lib/node_modules/neovim/bin/cli.js'
+if vim.fn.executable(node_host) == 1 then
+  vim.g.node_host_prog = node_host
 else
-  vim.g.loaded_node_provider = 0
+  vim.g.node_host_prog = 0
 end
 
 -- ruby
-if vim.fn.executable 'neovim-ruby-host' then
+if vim.fn.executable 'neovim-ruby-host' == 1 then
   vim.g.ruby_host_prog = vim.fn.exepath 'neovim-ruby-host'
 else
   vim.g.loaded_ruby_provider = 0
