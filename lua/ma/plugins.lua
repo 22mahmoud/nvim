cba1e6bf27b759c1aef8b1792db3bd63a522d781
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2026-02-20 23:11:57 +0200

    enhance plugins.lua

--- lua/ma/plugins.lua
+++ lua/ma/plugins.lua
@@ -1,27 +1,32 @@
 local cmd = vim.api.nvim_create_user_command
 
+--- @param path string
+local function gh(path) return ('https://github.com/%s'):format(path) end
+
+--- @type (string|vim.pack.Spec)[]
 local plugins = {
   -- colors/ui
-  { src = 'https://github.com/RRethy/nvim-base16', name = 'nvim-base16' },
-  { src = 'https://github.com/nvim-mini/mini.icons', name = 'nvim-mini-icons' },
+  gh 'RRethy/nvim-base16',
+  gh 'nvim-mini/mini.icons',
 
   -- editor,
-  { src = 'https://github.com/nvim-mini/mini.surround', name = 'nvim-mini-surround' },
-  { src = 'https://github.com/stevearc/oil.nvim', name = 'nvim-oil' },
+  gh 'nvim-mini/mini.surround',
+  gh 'stevearc/oil.nvim',
 
   -- treesitter
-  {
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-    name = 'nvim-treesitter',
-    version = 'main',
-  },
-  {
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
-    name = 'nvim-treesitter-textobjects',
-    version = 'main',
-  },
+  gh 'nvim-treesitter/nvim-treesitter',
+  gh 'nvim-treesitter/nvim-treesitter-textobjects',
 }
 
 vim.pack.add(plugins)
 
 cmd('PkgUpdate', function() vim.pack.update(nil, { target = 'lockfile' }) end, {})
+cmd('PkgClean', function()
+  vim.pack.del(
+    vim
+      .iter(vim.pack.get())
+      :filter(function(x) return not x.active end)
+      :map(function(x) return x.spec.name end)
+      :totable()
+  )
+end, {})
